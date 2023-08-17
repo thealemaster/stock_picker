2125d2c4cd80a81c46df7d9ec00ddb0e62a1cbde
John Huston <john.huston@icloud.com>
2023-08-16 22:45:43 -0400

    Tests pass

--- stock_picker.py
+++ stock_picker.py
@@ -4,5 +4,8 @@ def calculate_profit (stockList) :
         print (stockList)
         print (max(stockList) - min(stockList))
 
+        if stockList.index(max(stockList)) == 0:
+                return 0
+        
         return max(stockList) - min (stockList)
 
