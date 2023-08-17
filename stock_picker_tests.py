21374301abc9a7545c82af29780020f5171945d2
John Huston <john.huston@icloud.com>
2023-08-16 22:58:07 -0400

    Max number in second position - failing test

--- stock_picker_tests.py
+++ stock_picker_tests.py
@@ -8,6 +8,12 @@ class TestProfitCalculation (unittest.TestCase):
         result = calculate_profit (price_list)
         self.assertEqual (result, 0)
 
+    def test_max_val_as_second_price_returns_second_value_minus_first_value(self):
+        price_list = [4, 5, 3, 2, 1]
+        result = calculate_profit (price_list)
+        self.assertEqual (result, 1)
+        
+
 if __name__ == '__main__':
     unittest.main()
 
