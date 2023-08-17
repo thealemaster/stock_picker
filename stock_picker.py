d56437940df4c029af405b1a1ee7250b1890a3f7
John Huston <john.huston@icloud.com>
2023-08-17 08:14:12 -0400

    Refactored to get away from the min and max approach

--- stock_picker.py
+++ stock_picker.py
@@ -4,26 +4,37 @@ def calculate_profit (stockList) :
         print (stockList)
         print (max(stockList) - min(stockList))
 
-        if stockList.index(max(stockList)) == 0:
-                return 0
+        lowPrice = stockList[0]
+        maxProfit = 0
+        
+        if stockList.index(max(stockList)) >= stockList.index(min(stockList)):
+                return max(stockList) - min(stockList)
+        else:
+                for index in range(len(stockList)):
+                        if stockList[index] < lowPrice:
+                                lowPrice = stockList[index]
+                        elif stockList[index] - lowPrice > maxProfit:
+                                maxProfit = stockList[index] - lowPrice
+        return maxProfit
 
-        if stockList.index(max(stockList)) == 1:
-                return max(stockList) - stockList[0]
+                                                             
+#        if stockList.index(max(stockList)) == 1:
+#                return max(stockList) - stockList[0]
 
-        if stockList.index(max(stockList)) == 2:
-                if stockList[0] <= stockList[1]:
-                        return max(stockList) - stockList[0]
-                else:
-                        return max(stockList) - stockList[1]
+#        if stockList.index(max(stockList)) == 2:
+#                if stockList[0] <= stockList[1]:
+#                        return max(stockList) - stockList[0]
+#                else:
+#                        return max(stockList) - stockList[1]
                 
-        if stockList.index(max(stockList)) == 3:
-                if stockList[0] <= stockList[1]:
-                        return max(stockList) - stockList[0]
-                else:
-                        if stockList[1] <= stockList[2]:
-                                return max(stockList) - stockList[1]
-                        else:
-                                return max(stockList) - stockList[2] 
+#        if stockList.index(max(stockList)) == 3:
+#                if stockList[0] <= stockList[1]:
+#                        return max(stockList) - stockList[0]
+#                else:
+#                        if stockList[1] <= stockList[2]:
+#                                return max(stockList) - stockList[1]
+#                        else:
+#                                return max(stockList) - stockList[2] 
 
         return max(stockList) - min (stockList)
 
--- stock_picker_tests.py
+++ stock_picker_tests.py
@@ -3,7 +3,7 @@ import unittest
 from stock_picker import calculate_profit
 
 class TestProfitCalculation (unittest.TestCase):
-    def test_max_val_as_first_price_returns_zero(self):
+    def test_no__returns_zero(self):
         price_list = [5, 4, 3, 2, 1]
         result = calculate_profit (price_list)
         self.assertEqual (result, 0)
