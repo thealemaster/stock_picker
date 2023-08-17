ce4a7017fba0bc7593f18a34d5e77f5fae85ed46
John Huston <john.huston@icloud.com>
2023-08-16 23:10:17 -0400

    Max number in second position only uses what's in first position

--- stock_picker.py
+++ stock_picker.py
@@ -6,6 +6,10 @@ def calculate_profit (stockList) :
 
         if stockList.index(max(stockList)) == 0:
                 return 0
+
+        if stockList.index(max(stockList)) == 1:
+                return max(stockList) - stockList[0]
         
+
         return max(stockList) - min (stockList)
 
