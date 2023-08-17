ce614641eb776e0ff8efd8164c5623e69d7e19b8
John Huston <john.huston@icloud.com>
2023-08-16 23:33:23 -0400

    Another failing test

--- stock_picker_tests.py
+++ stock_picker_tests.py
@@ -13,10 +13,15 @@ class TestProfitCalculation (unittest.TestCase):
         result = calculate_profit (price_list)
         self.assertEqual (result, 1)
 
-    def test_max_val_as_third_price_uses_first_and_second_value_as_minimum(self):
+    def test_max_val_as_third_price_uses_first_and_second_value_as_possible_minimum(self):
         price_list = [4, 3, 5, 2, 1]
         result = calculate_profit (price_list)
         self.assertEqual (result, 2)
+        
+    def test_max_val_as_fourth_price_uses_first_thru_third_as_possible_minimum(self):
+        price_list = [4, 3, 2, 5, 1]
+        result = calculate_profit (price_list)
+        self.assertEqual (result, 3)
         
 
 if __name__ == '__main__':
